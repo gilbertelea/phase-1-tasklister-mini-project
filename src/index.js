625fe8db066c2dedc2179a93fd8d4228c456c42f
gilbert <eleazarbiboum@gmail.com>
2022-05-07 22:50:43 -0400

    Assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    Handletask(e.target.new_task_description.value);
+    HandleFirst(e.target.fname.value);
+    // HandleSecond(e.target.lname.value);
+    form.reset()
+  })
+
 });
+
+function Handletask(task){
+  let  p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', HandleDelete);
+  p.textContent = `${task } `;
+  btn.textContent = 'x'
+  p.appendChild(btn);
+  const Task = document.querySelector('#tasks');
+  Task.appendChild(p);
+  
+}
+
+function HandleFirst(fn){
+  let  p = document.createElement('p');
+  p.textContent = `${fn } `;
+  const prenom = document.querySelector('#prenom');
+  prenom.appendChild(p)
+  
+}
+
+// function HandleSecond(ln){
+//   let  p = document.createElement('p');
+//   p.textContent = `${ln } `;
+//   const nom = document.querySelector('#nom');
+//   nom.appendChild(p)
+// }
+
+
+
+function HandleDelete(e){
+  e.target.parentNode.remove()
+}
+
